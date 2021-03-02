c159b0bea83927de017f803a25a23b906845f9da
Robert Kristian Alm <exarchiasghost@gmail.com>
2021-03-03 00:35:52 +0100

    initial code.
    Working node.js and express.js (plus other stuff). It is actually my code from the task 4 from lab 1

---
+++ bonline/server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const mysql = require('mysql');
+var theJson;
+var msg;
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    //using a 10 secons interval. it continuesly in a loop.
+    setInterval(sendResponseDb, 10000, dbcon);
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//file already exists
+app.get('/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+//file is created on the fly.
+app.get('/data.txt', function(req, res) {
+    res.send(msg)
+    console.log("sending temperatures");
+});
+
+app.listen(8080);
+
+const dbcon = mysql.createConnection({
+	host: "den1.mysql5.gear.host",
+	user: "xtracker",
+	password: "Kt4j_?V18w076",
+	database: "xtracker"
+	});
+	
+	
+function sendResponseDb(dbcon) {
+    console.log("updating the database");
+	dbcon.query('select * from temperatures;', (err, result) => {
+		if(err)
+			throw err;
+		
+		
+		theJson = result;
+	});
+    if(theJson != null){
+        msg="<table><tr><th>id</th><th>temperature</th><th>timestamp</th></tr>";
+        for(x=0; x<5; x++){
+            msg = msg + "<tr><td>" + theJson[x].id + "</td><td>"  + theJson[x].temperature + "</td><td>" 
+            + theJson[x].timestamp + "</td></tr>";
+        }
+        msg = msg + "</table>";
+    }
+
+}
+
